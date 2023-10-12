d34153d878ea0b7b011cbfdbc1c5214bbe5da415
freetimezp <kraftukrnet@ukr.net>
2023-10-12 23:14:48 +0300

    06. js-halloween-02. Animation use GSAP.

--- assets/js/main.js
+++ assets/js/main.js
@@ -34,3 +34,23 @@ const scrollHeader = () => {
 window.addEventListener('scroll', scrollHeader);
 
 /*=============== GSAP ANIMATION ===============*/
+gsap.from('.home__shadow', 1.5, { opacity: 0, y: -300, delay: 0.2 });
+gsap.from('.home__points', 1.5, { opacity: 0, y: -300, delay: 0.4 });
+gsap.from('.home__grass', 1.5, { opacity: 0, y: 300, delay: 0.2, ease: 'elastic.out(1, 0.5)' });
+gsap.from('.home__pumpkin', 1.5, { opacity: 0, y: -300, delay: 0.6, ease: 'bounce.out' });
+gsap.from('.home__stones', 1.5, { opacity: 0, y: 300, delay: 0.8, ease: 'elastic.out(1, 0.5)' });
+gsap.from('.home__moon', 1.5, { opacity: 0, y: 300, delay: 1.5, ease: 'elastic.out(1, 0.5)' });
+gsap.from('.home__titles', 1.5, { opacity: 0, y: -300, delay: 1.5, ease: 'elastic.out(1, 0.5)' });
+gsap.from('.home__trees', 1.5, { opacity: 0, y: 300, delay: 2, ease: 'elastic.out(1, 0.5)' });
+gsap.from('.home__data', 1.5, { opacity: 0, y: 200, delay: 2, ease: 'elastic.out(1, 0.5)' });
+
+
+
+
+
+
+
+
+
+
+
